3e29228a608c2bb52fa28b37bc9fd2e75345b854
caoyi <caoyi@yanrongyun.com>
2020-11-19 10:44:28 +0800

    ahead tool update

---
+++ aheadtest/run_ahead.sh
@@ -0,0 +1,26 @@
+	
+for cache in native none;do
+    sed -i "s/tuneFile.*/tuneFileCacheType             = ${cache}/" /etc/yrfs/yrfs-client.conf
+    cat /etc/yrfs/yrfs-client.conf|grep tuneFileCacheType
+    while true;do
+        echo -e "stop client"
+        systemctl stop yrfs-client
+        status=`lsmod|grep yrfs`
+        mnt=`findmnt /mnt/yrfs`
+        sleep 3
+        if [[ -z $status ]] && [[ -z $mnt ]];then
+            echo -e "start client"
+            systemctl start yrfs-client
+    	    mnt=`findmnt /mnt/yrfs`
+            if [[ -n $mnt ]];then
+                echo -e "cache type: $cache script run" 
+  		python ./fiotest.py
+    	    	break
+            fi
+        else
+            continue
+        fi
+    done
+done
+#sed 's/tuneFile.*/tuneFileCacheType             = native/' /etc/yrfs/yrfs-client.conf
+#sed 's/tuneFile.*/tuneFileCacheType             = none/' /etc/yrfs/yrfs-client.conf
